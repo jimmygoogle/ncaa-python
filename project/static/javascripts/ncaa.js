ea4042d3b8fac99bf98f14a140fe30fb871e9069
jimmygoogle <jim@jimandmeg.com>
2019-01-10 16:18:19 -0500

    Added logic to auto picking games as well as UI for it

--- project/static/javascripts/ncaa.js
+++ project/static/javascripts/ncaa.js
@@ -10,15 +10,250 @@ $(document).ready (
     $('li').click(function() {
       setUserPick($(this));
     });
+    
+    $('#chalk').click(function(event){
+      event.preventDefault();
+      makePicks('chalk');
+    });
 
+    $('#mix').click(function(event){
+      makePicks('mix');
+    });
+
+    $('#random').click(function(event){
+      makePicks('random');
+    });
     // submit form to set/check user pool name
-    $("#poolNameForm").submit( function(event) {
+    $('#poolNameForm').submit(function(event) {
       event.preventDefault();
       checkUserPool();
+    });
+    
+    // validate and submit user bracket form
+    $('#submit_user_bracket').click(function(event) {
+      event.preventDefault();
+      validateUserInput();
     });
   }
 );
 
+function makePicks(type) {
+  let game = '';
+  let games = [];
+  
+  // added some weighting for seeds by round
+  // https://www.betfirm.com/seeds-national-championship-odds/
+  const weights = {
+    1 : {
+      1 : 0.999999,    
+      2 : 0.939,
+      3 : 0.841,
+      4 : 0.803,
+      5 : 0.644,
+      6 : 0.629,
+      7 : 0.614,
+      8 : 0.508,
+      9 : 0.492,
+      10 : 0.386,
+      11 : 0.371,
+      12 : 0.356,
+      13 : 0.197,
+      14 : 0.159,
+      15 : 0.061,
+      16 : 0.000001
+    },
+    2 : {
+      1 : 0.864,
+      2 : 0.629,
+      3 : 0.508,
+      4 : 0.47,
+      5 : 0.326,
+      6 : 0.326,
+      7 : 0.189,
+      8 : 0.098,
+      9 : 0.038,
+      10 : 0.182,
+      11 : 0.152,
+      12 : 0.152,
+      13 : 0.045,
+      14 : 0.015,
+      15 : 0.008,
+      16 : 0.0
+    },
+    3 : {
+      1 : 0.697,
+      2 : 0.462,
+      3 : 0.242,
+      4 : 0.159,
+      5 : 0.061,
+      6 : 0.106,
+      7 : 0.076,
+      8 : 0.061,
+      9 : 0.015,
+      10 : 0.061,
+      11 : 0.053,
+      12 : 0.008,
+      13 : 0.0,
+      14 : 0.0,
+      15 : 0.0,
+      16 : 0.0
+    },
+    4 : {
+      1 : 0.409,
+      2 : 0.212,
+      3 : 0.114,
+      4 : 0.098,
+      5 : 0.045,
+      6 : 0.023,
+      7 : 0.023,
+      8 : 0.38,
+      9 : 0.008,
+      10 : 0.008,
+      11 : 0.023,
+      12 : 0.0,
+      13 : 0.0,
+      14 : 0.0,
+      15 : 0.0,
+      16 : 0.0
+    },
+    5 : {
+      1 : 0.242,
+      2 : 0.098,
+      3 : 0.068,
+      4 : 0.023,
+      5 : 0.023,
+      6 : 0.015,
+      7 : 0.015,
+      8 : 0.023,
+      9 : 0.0,
+      10 : 0.0,
+      11 : 0.0,
+      12 : 0.0,
+      13 : 0.0,
+      14 : 0.0,
+      15 : 0.0,
+      16 : 0.0
+    },
+    6 : {
+      1 : 0.152,
+      2 : 0.038,
+      3 : 0.03,
+      4 : 0.008,
+      5 : 0.088,
+      6 : 0.008,
+      7 : 0.008,
+      8 : 0.008,
+      9 : 0.0,
+      10 : 0.0,
+      11 : 0.0,
+      12 : 0.0,
+      13 : 0.0,
+      14 : 0.0,
+      15 : 0.0,
+      16 : 0.0
+    }
+  };
+  
+  // loop through all games and make picks based on user preference
+  for (let index = 1; index <= 63; index++) {
+    
+    teams = []
+    $('.game'+index).find('li').each(function(){
+      // get seed and team 
+      // ex: 1 Virginia
+      const teamInfo = $(this).html().split(' ');
+      const seedId = teamInfo[1]; 
+
+      if(!seedId) {
+        return true;
+      }
+
+      // get game and round
+      const gameData = $(this).parent().attr('class').split(' ');
+      const game = gameData[1].match(/game(\d+)/);
+      const round = getRound(game[1]);
+      
+      data = {seed_id: seedId, round: round, game: $(this)};
+      teams.push({seed_id: seedId, round: round, game: $(this)});
+    });
+    
+    if(teams.length) {
+      // sort based on seed (chalk, weighted, random)
+      teams.sort(function(a, b){
+        
+        switch(type) {
+          case 'random':
+            return 0.5 - Math.random();
+          case 'chalk':
+            return a.seed_id - b.seed_id;
+          case 'mix':
+            const data = {};
+            data[a.seed_id] = weights[a.round][a.seed_id];
+            data[b.seed_id] = weights[b.round][b.seed_id];
+
+            return weightedRandom(data);
+        }
+      });
+      
+      // set pick
+      //console.log('picking %s for game %s', teams[0].seed_id, index);
+      teams[0].game.trigger('click');
+    }    
+  }
+}
+
+function getRound(game) {
+  let round = 0;
+
+  if(game <= 32) {
+    round = 1;
+  }
+  else if(game >= 33 && game <= 48) {
+    round = 2;
+  }
+  else if(game >= 49 && game <= 56) {
+    round = 3;
+  }
+  else if(game >= 57 && game <= 60) {
+    round = 4;
+  }
+  else if(game == 61 || game == 62) {
+    round = 5;
+  }
+  else {
+    round = 6;
+  }
+
+  return round;
+}
+
+function weightedRandom(data) {
+  let index;
+  let sum = 0; 
+  
+  // determine max
+  const min = 0;
+  let max = 0;
+  for (index in data) {
+    max += data[index];
+  }
+  
+  // generate random number between min and max
+  const rand = Math.random() * (max - min) + min;
+  //console.log(data);
+  //console.log('min is %s and max is %s', min, max);
+  //console.log('rand is %s', rand);
+
+  for (index in data) {
+    sum += data[index];
+    //console.log('sum is %s', sum);
+    if (rand >= sum) return index;
+  }
+}
+
+//https://api.sportradar.us/ncaamb-t3/polls/US/2018/rankings.json?api_key=ub4xqkccmjr8gkmjmvchhtwr
+//https://api.sportradar.us/ncaamb-t3/polls/AP/2018/rankings.json?api_key=ub4xqkccmjr8gkmjmvchhtwr
+    
 function loadUserPicks() {
   const $userBracketInfoForm = $('#userBracketInfoForm');
   const bracketType = $userBracketInfoForm.find('#bracketType').val();
@@ -202,6 +437,11 @@ function setUserPick(obj) {
 
   // clear all previous picks (in case user is changing pick)
   clearPreviousPicks(gameData[1], userPickedTeam, gameSlots);
+
+  // show bracket form now that they have picked enough games
+  if((game[1] == 61 || game[1] == 62) && $('#userBracketInfoForm')) {
+    $('.bracket_details').removeClass('hidden');
+  }
 
   // set data so it can be submitted to DB
   setUserFormData(gameData[1], teamID);

8c14c005be0eac00bdb99f4d93fa74a42144b031
jimmygoogle <jim@jimandmeg.com>
2019-01-29 18:25:56 -0500

    Updated the application to use a factory per Flask docs

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from project import app, flask_session
+#from project import app, flask_session
+#
+#if __name__ == '__main__':
+#    app.config['SESSION_TYPE'] = 'filesystem'
+#
+#    flask_session.init_app(app)
+#    
+#    app.debug = True
+#    app.run(host='0.0.0.0')
 
-if __name__ == '__main__':
-    app.config['SESSION_TYPE'] = 'filesystem'
 
-    flask_session.init_app(app)
-    
-    app.debug = True
-    app.run(host='0.0.0.0')
+from project import create_app
+
+create_app().run(host='0.0.0.0', port=5000, debug=True)
--- project/__init__.py
+++ project/__init__.py
@@ -1,22 +1,39 @@
-from flask import Flask, request, session
-from flask_session import Session
-import datetime
+from flask import Flask, request, session, g
 
-app = Flask(__name__, instance_relative_config=True)
-app.config.from_pyfile('config.py')
+def create_app(test_config=None):
+    
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('config.py')
+    
+    from project.mysql_python import MysqlPython
+    import datetime
+    with app.app_context():
+        db = MysqlPython()
+        db.get_db()
+        
+        # set some global variables
+        g.year = datetime.datetime.now().year
 
-flask_session = Session()
+    from flask_session import Session
+    flask_session = Session()
+    app.config['SESSION_TYPE'] = 'filesystem'
+    flask_session.init_app(app)
+    
+    if __name__ == '__main__':
+        app.debug = True
+        app.run(host='0.0.0.0')
+    
 
-# set some global variables
-YEAR = datetime.datetime.now().year
+    
+    from project.pool.views import pool_blueprint
+    from project.standings.views import standings_blueprint
+    from project.bracket.views import bracket_blueprint
+    from project.admin.views import admin_blueprint
+    
+    # register the blueprints
+    app.register_blueprint(pool_blueprint)
+    app.register_blueprint(standings_blueprint)
+    app.register_blueprint(bracket_blueprint)
+    app.register_blueprint(admin_blueprint)
 
-from project.pool.views import pool_blueprint
-from project.standings.views import standings_blueprint
-from project.bracket.views import bracket_blueprint
-from project.admin.views import admin_blueprint
-
-# register the blueprints
-app.register_blueprint(pool_blueprint)
-app.register_blueprint(standings_blueprint)
-app.register_blueprint(bracket_blueprint)
-app.register_blueprint(admin_blueprint)
+    return app
